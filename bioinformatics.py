6551d43e1efb183cd8f0a471a758aa8118481c16
noreenlouisecb <noreenlouiseb15@gmail.com>
2016-03-06 05:59:33 +0800

    Added countSubstrPattern function

--- bioinformatics.py
+++ bioinformatics.py
@@ -12,3 +12,24 @@ def getHammingDistance(str1, str2):
         return "String length is zero(0). Invalid."
     else:
         return "Error! Strings are not equal!"
+
+def countSubstrPattern(original, pattern):
+    if(len(original) > 0):
+        counter = 0; checker = 0
+        for index in range(len(original)):
+            tempindex = index; checker = 0
+            for index2 in range(len(pattern)):
+                if(tempindex != len(original)):
+                    if(original[tempindex] != pattern[index2]):
+                        checker = 1
+                        break
+                    else:
+                        tempindex+=1
+                else:
+                    checker = 1
+                    break
+            if(checker == 0):
+                counter += 1
+        return counter
+    else:
+        return 0
